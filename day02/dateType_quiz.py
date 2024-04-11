9e29fe2a9eeb6ac9f6e8ff443e4e22c8f059b859
daeunjung1016 <166515991+daeunjung1016@users.noreply.github.com>
2024-04-11 21:51:39 +0900

    Add files via upload

---
+++ day02/dateType_quiz.py
@@ -0,0 +1,20 @@
+# 유저에게
+# 첫 번째 정수 입력:
+# 두 번째 정수 입력:
+# 받은 후 두 수의 합을 출력하는 프로그램 만들기
+# ex) 첫 번째 정수 입력: 5
+#     두 번째 정수 입력: 3
+#     결과: 8
+
+# num1 = input("첫 번째 정수 입력")
+# num2 = input("두 번째 정수 입력")
+# result = int(num1) + int(num2)
+# print(f"결과: {result}")
+
+# age = input("몇 살이십니까?")
+# year = 2024 - int(age) + 1
+# print(f"나이가 {age}살이라면, 출생연도는 {year}년입니다.")
+
+side = input("한 변의 길이는?")
+width = int(side) * int(side)
+print(f"한 변의 길이가 {side}라면, 넓이는 {width}")
---
+++ day02/review.py
@@ -0,0 +1,9 @@
+date = input("날짜?")
+event = input("사건?")
+print(f"어제는 {date}, 주요 사건은 {event}였습니다.")
+
+date = input("날짜")
+morning = input("아침 메뉴")
+lunch = input("점심 메뉴")
+evening = input("저녁 메뉴")
+print(f"오늘 아침은 {date}을 먹고, 점심은 {lunch}을, 저녁은 {evening}을 먹었습니다")
