178f580119b1e5fabb50d82118b64a989250f2ac
daeunjung1016 <166515991+daeunjung1016@users.noreply.github.com>
2024-04-16 21:46:41 +0900

    Add files via upload

---
+++ day03/index.py
@@ -0,0 +1,18 @@
+#산술, 비교, 대입, 논리
+#멤버십 연산자[in]
+coffee = "americano"
+print('a' in coffee) # True
+print('ameri' in coffee) # True
+text = """""" # 두 문장 이상의 글
+news = """"유강남, 심리적으로 힘들어 보여"...김태형 감독이 밝힌 3B 타격의 아쉬움 [잠실 현장]"""
+print("유강남" in news)
+
+#슬라이싱 연산자 [:]
+car = "tesla"
+print(car[0:3]) #tes
+print(car[1:3]) #es
+
+#인덱스 연산자 [[]]
+code = "python"
+print(code[0]) #p
+print(code[3]) #h
